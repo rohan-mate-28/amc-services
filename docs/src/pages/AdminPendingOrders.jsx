163689f3995ec9052adbb39cf2d836b1bf509c03
rohan-mate-28 <rohanmate157@gmail.com>
2025-06-27 11:51:08 +0530

    Fix CORS and cookie settings

--- docs/src/pages/AdminPendingOrders.jsx
+++ docs/src/pages/AdminPendingOrders.jsx
@@ -6,7 +6,7 @@ import { Loader2 } from "lucide-react";
 import { toast } from "sonner";
 import { useDispatch, useSelector } from "react-redux";
 import { setloadings, setPendingProduct } from "@/redux/pendingproductSlice";
-import { CUSTOMER_PRODUCT_API_END_POINT } from "@/Utils/constant.js";
+import { ADMIN_PRODUCT_API_END_POINT } from "@/Utils/constant.js";
 
 const AdminPendingOrders = () => {
   const dispatch = useDispatch();
@@ -17,7 +17,7 @@ const AdminPendingOrders = () => {
     const fetchPendingOrders = async () => {
       dispatch(setloadings(true));
       try {
-        const res = await axios.get(`${CUSTOMER_PRODUCT_API_END_POINT}/pendingproduct`, {
+        const res = await axios.get(`${ADMIN_PRODUCT_API_END_POINT}/pendingproduct`, {
           withCredentials: true,
         });
         dispatch(setPendingProduct(res.data.orders || [])); // ✅ Use `orders` not `products`
@@ -35,7 +35,7 @@ const AdminPendingOrders = () => {
     try {
       setConfirmingId(productId);
       const res = await axios.patch(
-        `${CUSTOMER_PRODUCT_API_END_POINT}/${productId}/confirm`,
+        `${ADMIN_PRODUCT_API_END_POINT}/${productId}/confirm`,
         {},
         { withCredentials: true }
       );
