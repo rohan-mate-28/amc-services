3a8d4acfdfe1cf9c00447799de63ec36b2cf8d15
rohan-mate-28 <rohanmate157@gmail.com>
2025-06-27 11:38:48 +0530

    Fix CORS and cookie settings

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://stellar-shortbread-f58043.netlify.app"
+  "https://chimerical-treacle-110cc2.netlify.app"
 ];
 
 app.use(
