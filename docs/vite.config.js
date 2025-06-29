c223666301c66ffdc1c1cddad419ea31b49ecaf6
rohan-mate-28 <rohanmate157@gmail.com>
2025-06-29 14:58:48 +0530

    added _ridirect file

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   resolve: {
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', 
+          dest: '.'              
+        }
+      ]
+    })
+  ],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: 'docs/dist',  
+    emptyOutDir: true,
   },
 })
